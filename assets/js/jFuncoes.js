4cc00922ead5859e9c9dfb178e6028e707baf930
PauloBaronista <paulo@rpm.com.br>
2020-11-20 12:26:17 -0300

    Timer

--- assets/js/jFuncoes.js
+++ assets/js/jFuncoes.js
@@ -1,41 +1,26 @@
 $(document).ready(function () {
-
-    $('#modalCabore').modal();
-
-    //  Check if the user already accepted it
-    // if (window.localStorage.getItem('accept_modal')) {
-
-    //     $('#modalCabore').modal('hide');
-
-    //     return false;
-    // }
-    // $(".close").click(function () {
-
-    //     Save on LocalStorage
-    //     window.localStorage.setItem('accept_modal', true);
-
-    //     $('#modalCabore').modal('hide');
-
-    //     return false;
-    // });
+    $('#modalTimer').modal();
 }); 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-	$('#cookies').show();
+    $('#cookies').show();
+    $('body').css('overflow', 'hidden');
 
     // Check if the user already accepted it
     if (window.localStorage.getItem('accept_cookies')) {
-		$('#cookies').hide();
-		return false;
+        $('#cookies').hide();
+        $('body').css('overflow', 'auto');
+        return false;
     }
 
-    $(".btn_cookie").click(function(){
+    $(".btn_cookie").click(function () {
         // Save on LocalStorage
         window.localStorage.setItem('accept_cookies', true);
-		$('#cookies').hide();
-		return false;
-    }); 
+        $('#cookies').hide();
+        $('body').css('overflow', 'auto');
+        return false;
+    });
 });
 
 
